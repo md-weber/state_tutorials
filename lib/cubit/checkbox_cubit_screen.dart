25b975cb03257f9b5994538e7ec75a4795802b81
myracledesign <weber.max.1988@live.de>
2020-07-06 11:10:44 +0200

    updated the cubit files

--- lib/cubit/checkbox_cubit_screen.dart
+++ lib/cubit/checkbox_cubit_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cubit/flutter_cubit.dart';
 import 'package:smtutorial/widgets/drinks_widget.dart';
 
 import '../constants.dart';
--- lib/cubit/cubit_observer.dart
+++ lib/cubit/cubit_observer.dart
@@ -1,11 +1,2 @@
-import 'package:cubit/cubit.dart';
-
 // TODO Optional 2: Create a Observer and extend it from CubitObserver
 // TODO Optional 3: Override the onTransition method
-class MainCubitObserver extends CubitObserver {
-  @override
-  void onTransition(Cubit cubit, Transition transition) {
-    print("CubitObserver $transition");
-    super.onTransition(cubit, transition);
-  }
-}
