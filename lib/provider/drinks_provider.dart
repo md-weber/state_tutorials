20c2a44c43407339381591015efb8e0ef33d04cc
myracledesign <weber.max.1988@live.de>
2020-07-17 13:07:24 +0200

    removed the solution from the tutorial

--- lib/provider/drinks_provider.dart
+++ lib/provider/drinks_provider.dart
@@ -4,26 +4,3 @@
 // TODO 4: Write a get method for the drinks
 // TODO 10: Implement the selectDrink method
 // TODO 12: Implement a getter function for selected Drinks
-
-import 'package:flutter/foundation.dart';
-import 'package:smtutorial/models/drink.dart';
-
-class DrinksProvider extends ChangeNotifier {
-  List<Drink> _drinks = [
-    Drink("Water", false),
-    Drink("Cuba Libre", false),
-    Drink("Pina Colada", false),
-    Drink("Havana Cola", false)
-  ];
-
-  void selectDrink(Drink drink, bool selected) {
-    _drinks.firstWhere((element) => element.name == drink.name).selected =
-        selected;
-    notifyListeners();
-  }
-
-  List<Drink> get selectedDrinks =>
-      _drinks.where((element) => element.selected).toList();
-
-  List<Drink> get drinks => _drinks;
-}
