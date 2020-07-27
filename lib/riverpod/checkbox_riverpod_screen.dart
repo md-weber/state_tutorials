28777e814004048808470fc64dff99adc0c6c76a
myracledesign <weber.max.1988@live.de>
2020-07-27 14:40:34 +0200

    Added all todos for Riverpod

--- lib/riverpod/checkbox_riverpod_screen.dart
+++ lib/riverpod/checkbox_riverpod_screen.dart
@@ -4,11 +4,11 @@ import 'package:smtutorial/widgets/drinks_widget.dart';
 
 import '../constants.dart';
 
-// TODO 7: Create a final variable with a StateNotifierProvider
-// TODO 8: Initialize the DrinkStateNotifier with a List of drinks
+// TODO 5: Create a final variable with a StateNotifierProvider
+// TODO 6: Initialize the DrinkStateNotifier with a List of drinks
 
-// TODO 9: Initialize a allDrinks variable
-// TODO 10: Use the Computed Method of Riverpod to read the state of drinksProvider
+// TODO 7: Initialize a allDrinks variable
+// TODO 8: Use the Computed Method of Riverpod to read the state of drinksProvider
 
 class CheckboxRiverpodScreen extends StatelessWidget {
   @override
@@ -25,17 +25,17 @@ class CheckboxRiverpodScreen extends StatelessWidget {
             child: Container(
               padding: EdgeInsets.all(8.0),
               decoration: kWhiteBackground,
-              // TODO 11: Use the Riverpod Consumer
+              // TODO 9: Use the Riverpod Consumer
               child: Column(children: [
                 Text(
                   "Drinks tonight",
                   style: Theme.of(context).textTheme.headline4,
                 ),
-                // TODO 12: Write a for loop to access all drinks via read of the computed function
+                // TODO 10: Write a for loop to access all drinks via read of the computed function
                 DrinksWidget(
                   drink: Drink("Replace", false),
                   onChanged: (bool value) {
-                    // TODO 13: Access the drinksProvider.read and selectDrink
+                    // TODO 11: Access the drinksProvider.read and selectDrink
                   },
                 ),
                 Text(
@@ -47,11 +47,11 @@ class CheckboxRiverpodScreen extends StatelessWidget {
                     itemBuilder: (context, index) {
                       return ListTile(
                         title: Text(
-                            // TODO 14: read drinksProvider and get the selectedDrinks
+                            // TODO 12: read drinksProvider and get the selectedDrinks
                             ""),
                       );
                     },
-                    // TODO 15: use the read drinksProvider to receive the selectedDrinks
+                    // TODO 13: use the read drinksProvider to receive the selectedDrinks
                     itemCount: 0,
                   ),
                 ),
--- lib/riverpod/drink_state_notifier.dart
+++ lib/riverpod/drink_state_notifier.dart
@@ -1,3 +1,3 @@
-// TODO 1: Create a DrinksNotifier class that extends ChangeNotifier
-// TODO 2: Initialize a List<Drink>
-// TODO 5: Add the methods to select a Drink, get the selected drinks, get all drinks
+// TODO 2: Create a DrinksNotifier class that extends ChangeNotifier
+// TODO 3: Initialize a List<Drink>
+// TODO 4: Add the methods to select a Drink, get the selected drinks, get all drinks
