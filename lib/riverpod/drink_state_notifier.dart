82d8fba330c28e09967ce524cb266ca192200165
myracledesign <weber.max.1988@live.de>
2020-08-25 12:23:44 +0200

    update todo in drink_state_notifier.dart

--- lib/riverpod/drink_state_notifier.dart
+++ lib/riverpod/drink_state_notifier.dart
@@ -1,3 +1,3 @@
 // TODO 2: Create a DrinkStateNotifier class that extends StateNotifier
 // TODO 3: Create the super notification
-// TODO 4: Add the methods to select a Drink, get the selected drinks, get all drinks
+// TODO 4: Add the method to select a Drink
