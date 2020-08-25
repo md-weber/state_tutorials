710c7bf768ad4145c40c79e321e166187a286317
myracledesign <weber.max.1988@live.de>
2020-08-25 15:27:47 +0200

    fixed the solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:smtutorial/riverpod/checkbox_riverpod_screen.dart';
 
 void main() {
