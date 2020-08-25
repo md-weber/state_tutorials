1bec97e5b0e3c7a12e79a5737cecfa20d0b129f3
myracledesign <weber.max.1988@live.de>
2020-08-25 04:38:05 +0200

    removed the StateNotifier class

--- lib/riverpod/drink_state_notifier.dart
+++ lib/riverpod/drink_state_notifier.dart
@@ -1,9 +1,3 @@
-// TODO 2: Create a [DrinkStateNotifier] class that extends [StateNotifier]
+// TODO 2: Create a DrinkStateNotifier class that extends StateNotifier
 // TODO 3: Create the super notification
 // TODO 4: Add the methods to select a Drink, get the selected drinks, get all drinks
-import 'package:smtutorial/models/drink.dart';
-import 'package:state_notifier/state_notifier.dart';
-
-class DrinkStateNotifier extends StateNotifier<List<Drink>> {
-  DrinkStateNotifier(List<Drink> state) : super(state);
-}
