8a4a1f2a5302bd18c78facbae654868941479991
Max <flutterexp@gmail.com>
2020-11-19 21:44:31 +0100

    removed the updatedrink action

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -1,13 +1,5 @@
-import 'package:smtutorial/models/drink.dart';
-
 // Actions: Actions are payloads of information that send data from your
 // application to your store. They are the only source of information
 // for the store. You send them to the store using store.dispatch().
 
 // TODO 3: Create a UpdateDrinkAction with a payload "updatedDrink"
-
-class UpdateDrinkAction {
-  final Drink updatedDrink;
-
-  UpdateDrinkAction(this.updatedDrink);
-}
