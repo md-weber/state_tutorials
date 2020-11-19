9e547e7f3c1155d7d58d0ac3a89c902489bbfd73
Max <flutterexp@gmail.com>
2020-11-19 21:43:59 +0100

    removed the appstate

--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -1,8 +1,1 @@
-import 'package:smtutorial/models/drink.dart';
-
 // TODO 2: create an AppState with an empty List of Drinks
-class AppState {
-  List<Drink> drinks;
-
-  AppState({this.drinks = const []});
-}
