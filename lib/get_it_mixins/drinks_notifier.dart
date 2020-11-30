b92cac74860d42a155ccd61e4c7a6373fddea239
Max <flutterexp@gmail.com>
2020-11-30 21:25:00 +0100

    removed the solution

--- lib/get_it_mixins/drinks_notifier.dart
+++ lib/get_it_mixins/drinks_notifier.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/widgets.dart';
-import 'package:smtutorial/models/drink.dart';
-
 // TODO 2: Create a GetItDrinksNotifier extends ChangeNotifier
-
-class GetItDrinksNotifier extends ChangeNotifier {
-  List<Drink> _drinks = [
-    Drink("Water", false),
-    Drink("Cuba Libre", false),
-    Drink("Pina Colada", false),
-    Drink("Havana Cola", false)
-  ];
-
-  List<Drink> get selectedDrinks =>
-      _drinks.where((element) => element.selected).toList();
-
-  List<Drink> get drinks => _drinks;
-}
