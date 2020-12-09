36bea0c3f1d58991451ee6d9339a481a51ac294b
Max <flutterexp@gmail.com>
2020-12-09 08:24:50 +0100

    removed the other projects from get_it_tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
-import 'package:smtutorial/models/drink.dart';
-import 'package:smtutorial/redux/app_state.dart';
-import 'package:smtutorial/redux/reducer.dart';
-import 'package:smtutorial/redux/redux_screen.dart';
+import 'package:smtutorial/get_it_mixins/get_it_mixin_screen.dart';
 
 void main() {
   // TODO 5: call the setup method
@@ -12,29 +7,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final Store<AppState> _store = Store<AppState>(
-    updateDrinksReducer,
-    initialState: AppState(
-      drinks: [
-        Drink("Water", false),
-        Drink("Cuba Libre", false),
-        Drink("Pina Colada", false),
-        Drink("Havana Cola", false)
-      ],
-    ),
-  );
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Redux solution - Flutter Explained',
+      title: 'Get It Mixins Tutorial - Flutter Explained',
       theme: ThemeData.light().copyWith(primaryColor: Color(0xFFe13133)),
-      home: StoreProvider<AppState>(
-        store: _store,
-        child: ReduxScreen(),
-      ),
+      home: GetItMixinScreen(),
     );
   }
 }
